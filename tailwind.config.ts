95e8a4ba18bff35abae6bbf5045f76e4374f8d58
CaffeinePower <cffnpwr@gmail.com>
2023-05-08 10:14:18 +0900

    update: tailwind.configがtsをサポートしたので切り替え

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const tailwindConfig: Config = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -11,3 +12,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default tailwindConfig;
