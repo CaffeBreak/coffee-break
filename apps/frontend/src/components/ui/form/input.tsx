748518cacc5562d7f71bf75f478a83054c4b2b09
AOI_1up <aoi_1up@icloud.com>
2023-08-05 09:53:30 +0900

    feat: ホーム画面の実装

---
+++ apps/frontend/src/components/ui/form/input.tsx
@@ -0,0 +1,9 @@
+export const Input = () => {
+  return (
+    <input
+      type="text"
+      placeholder="名前を入力して焼きあがる"
+      className="h-[60px] w-[450px] rounded border-b-8 border-yellow-900 bg-yellow-500 px-2 text-center text-3xl text-white placeholder:text-white"
+    />
+  );
+};
---
+++ apps/frontend/src/components/ui/global/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="h-[70px] w-full bg-yellow-800">
+      <div></div>
+    </header>
+  );
+};
