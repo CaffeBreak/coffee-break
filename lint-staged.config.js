c033867a2bfdd115a5fb03400df743ff42f7e695
cffnpwr <cffnpwr@gmail.com>
2023-08-05 17:35:23 +0000

    fix: ワークスペースに応じてlintをするように変更

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,13 +2,15 @@ import * as path from "path";
 
 export default {
   "apps/**/*.{js,jsx,ts,tsx}": ["pnpm tsc --noEmit --allowJs --checkJs"],
-  "apps/**/*.{js,jsx,ts,tsx,json,css,scss}": "pnpm format",
+  "apps/**/*.{js,jsx,ts,tsx,json,css,scss}":
+    "pnpm prettier --write --ignore-path .gitignore",
   "apps/frontend/**/*.{jsx,tsx}": (filenames) => {
-    const cmdForNextjs = `pnpm lint:fix --file ${filenames
+    const cmdForNextjs = `pnpm --filter frontend next lint --file ${filenames
       .map((f) => path.relative(process.cwd(), f))
       .join(" --file ")}`;
 
     return [cmdForNextjs];
   },
-  "apps/backend/**/*.{js,ts}": "pnpm lint:fix",
+  "apps/backend/**/*.{js,ts}":
+    "pnpm --filter backend eslint --ignore-path .gitignore",
 };
