35541f0f984a0bc8d2ae6301ee1d7b4573bfcd94
cffnpwr <cffnpwr@gmail.com>
2023-08-05 12:02:38 +0000

    feat: プレイヤーと部屋のリポジトリの定義

---
+++ apps/backend/src/error/repository.ts
@@ -0,0 +1,15 @@
+export class RepositoryError extends Error {
+  static {
+    this.prototype.name = "RepositoryError";
+  }
+  constructor(message: string, options?: ErrorOptions) {
+    super(message, options);
+
+    // Error.captureStackTraceはV8 Engineのみに存在する
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+    Object.setPrototypeOf(this, RepositoryError.prototype);
+  }
+}
