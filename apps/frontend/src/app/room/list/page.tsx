7f54f8af8187d8e5b019d83d0784c284842c4eb1
cffnpwr <cffnpwr@gmail.com>
2023-10-13 04:36:19 +0000

    feat: ts-resultsをesmに対応させた

--- apps/frontend/src/app/room/list/page.tsx
+++ apps/frontend/src/app/room/list/page.tsx
@@ -4,13 +4,13 @@ import { RoomMember } from "@/components/ui/roomMember";
 
 const RoomList = () => {
   return (
-    <div className="w-full h-2/3 flex">
-      <div className="flex w-1/2 h-full flex-col items-center justify-center space-y-10">
+    <div className="flex h-2/3 w-full">
+      <div className="flex h-full w-1/2 flex-col items-center justify-center space-y-10">
         <ControlRoom msg="部屋を解散" type="end" />
         <RoomInfo />
         <ControlRoom msg="ゲームの開始" type="start" />
       </div>
-      <div className="flex w-1/2 h-full flex-col items-center justify-center space-y-10">
+      <div className="flex h-full w-1/2 flex-col items-center justify-center space-y-10">
         <RoomMember />
       </div>
     </div>
---
+++ packages/ts-results/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '/test/.*.test.ts',
+    globals: {
+        'ts-jest': {
+            tsconfig: 'test/tsconfig.json',
+        },
+    },
+    collectCoverageFrom: ['src/**/*.ts'],
+    collectCoverage: true,
+    coverageThreshold: {
+        './src/**/*.ts': {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
+};
