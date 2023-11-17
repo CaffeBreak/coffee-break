dc1afe4f464c9cf134608165c937d54c3a65ae18
AOI_1up <aoi_1up@icloud.com>
2023-11-17 04:52:53 +0000

    feat: 焼きあがれる用になった

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,48 @@
-import { Enter } from "@/components/ui/button/enter";
-import { InputText } from "@/components/ui/form/inputText";
+"use client";
+import { useSetAtom } from "jotai";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import { Player } from "@/components/atom/Player";
+import { trpc } from "@/utils/trpc";
 
 const Home = () => {
+  const player = trpc.player.create.useMutation();
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [showError, setShowError] = useState(false);
+  const setPlayer = useSetAtom(Player);
+
+  const handleClick = () => {
+    player.mutate(
+      {
+        name: name,
+      },
+      {
+        onError: () => {
+          setShowError(true);
+        },
+        onSuccess: (data) => {
+          setPlayer({
+            id: data.id,
+            name: data.name,
+            role: data.role,
+            status: data.status,
+            roomId: "",
+          });
+        },
+      },
+    );
+  };
+
+  useEffect(() => {
+    if (player.data) {
+      console.log(player.data);
+      router.push("/room");
+    }
+  }, [player, router]);
+
   return (
     <>
       <div className="my-[70px] flex w-full flex-col items-center justify-center">
@@ -9,10 +50,23 @@ const Home = () => {
         <div className="my-2 ml-[130px] font-serif text-8xl italic">Break</div>
       </div>
       <div className="flex w-full justify-center">
-        <InputText placeholder="名前を入力して焼きあがる" />
+        <input
+          type="text"
+          placeholder="名前を入力"
+          className="h-[60px] w-[450px] rounded border-b-8 border-yellow-900 bg-yellow-500 px-2 text-center text-3xl text-white placeholder:text-white"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div className="flex h-[30px] items-center justify-center text-red-600">
+        {showError && `「${name}」は残念ながら炭になりました`}
       </div>
       <div className="mt-[70px] flex w-full justify-center">
-        <Enter dest="/room" />
+        <button
+          onClick={handleClick}
+          className="h-[60px] w-[200px] rounded-3xl bg-yellow-500 text-3xl text-white"
+        >
+          焼きあがる
+        </button>
       </div>
     </>
   );
