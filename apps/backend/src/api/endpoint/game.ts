a3723c3b5f8a394eb0008fbc8175d98056ac2571
CaffeinePower <cffnpwr@gmail.com>
2024-01-07 09:35:54 +0000

    feat: スキップエンドポイントとゲームスタートエンドポイントとの干渉を修正

--- apps/backend/src/api/endpoint/game.ts
+++ apps/backend/src/api/endpoint/game.ts
@@ -37,6 +37,58 @@ export class GameRouter {
 
   public execute() {
     return router({
+      skipPhase: publicProcedure
+        .meta({ openapi: { method: "POST", path: "/game/skipPhase" } })
+        .input(skipPhaseSchema)
+        .output(z.object({}))
+        .mutation(async (opts) => {
+          const { input } = opts;
+          const { playerId } = input;
+
+          const playerIdResult = playerIdSchema.safeParse(playerId);
+          if (!playerIdResult.success) {
+            const errorOpts: ConstructorParameters<typeof TRPCError>[0] = {
+              code: "BAD_REQUEST",
+              cause: playerIdResult.error,
+            };
+
+            throw new TRPCError(errorOpts);
+          }
+
+          const result = await this.skipPhaseUseCase.execute(playerIdResult.data);
+          if (result.isErr()) {
+            const errorOpts = ((e: UseCaseError): ConstructorParameters<typeof TRPCError>[0] => {
+              if (e instanceof RepositoryOperationError) {
+                return {
+                  message: e.message,
+                  code: "INTERNAL_SERVER_ERROR",
+                  cause: e,
+                };
+              } else if (
+                e instanceof PlayerNotFoundError ||
+                e instanceof OperationNotAllowedError ||
+                e instanceof RoomNotFoundError ||
+                e instanceof PlayerNotJoinedRoomError
+              ) {
+                return {
+                  message: e.message,
+                  code: "BAD_REQUEST",
+                  cause: e,
+                };
+              } else {
+                return {
+                  message: "Something went wrong.",
+                  code: "INTERNAL_SERVER_ERROR",
+                  cause: e,
+                };
+              }
+            })(result.unwrapErr());
+
+            throw new TRPCError(errorOpts);
+          }
+
+          return {};
+        }),
       start: publicProcedure
         .meta({ openapi: { method: "POST", path: "/game/{roomId}" } })
         .input(startGameSchema)
@@ -88,58 +140,6 @@ export class GameRouter {
                 };
               } else {
                 return { message: e.message, code: "INTERNAL_SERVER_ERROR", cause: e };
-              }
-            })(result.unwrapErr());
-
-            throw new TRPCError(errorOpts);
-          }
-
-          return {};
-        }),
-      skipPhase: publicProcedure
-        .meta({ openapi: { method: "POST", path: "/game/skipPhase" } })
-        .input(skipPhaseSchema)
-        .output(z.object({}))
-        .mutation(async (opts) => {
-          const { input } = opts;
-          const { playerId } = input;
-
-          const playerIdResult = playerIdSchema.safeParse(playerId);
-          if (!playerIdResult.success) {
-            const errorOpts: ConstructorParameters<typeof TRPCError>[0] = {
-              code: "BAD_REQUEST",
-              cause: playerIdResult.error,
-            };
-
-            throw new TRPCError(errorOpts);
-          }
-
-          const result = await this.skipPhaseUseCase.execute(playerIdResult.data);
-          if (result.isErr()) {
-            const errorOpts = ((e: UseCaseError): ConstructorParameters<typeof TRPCError>[0] => {
-              if (e instanceof RepositoryOperationError) {
-                return {
-                  message: e.message,
-                  code: "INTERNAL_SERVER_ERROR",
-                  cause: e,
-                };
-              } else if (
-                e instanceof PlayerNotFoundError ||
-                e instanceof OperationNotAllowedError ||
-                e instanceof RoomNotFoundError ||
-                e instanceof PlayerNotJoinedRoomError
-              ) {
-                return {
-                  message: e.message,
-                  code: "BAD_REQUEST",
-                  cause: e,
-                };
-              } else {
-                return {
-                  message: "Something went wrong.",
-                  code: "INTERNAL_SERVER_ERROR",
-                  cause: e,
-                };
               }
             })(result.unwrapErr());
 
