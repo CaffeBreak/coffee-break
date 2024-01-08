986b07fb5ed977f867dc99a6bd3cb5a3aca6f999
yanbaru1331 <m19034@g.metro-cit.ac.jp>
2024-01-08 03:56:47 +0000

    NPCスキップ気合の実装 突貫工事なので後で細かい部分修正

--- apps/npc/src/__main__.py
+++ apps/npc/src/__main__.py
@@ -1,7 +1,7 @@
 import asyncio
 from urllib import response
 import websockets
-import json, sys, datetime
+import json, sys, datetime, aiohttp
 
 from multiprocessing import Process
 from time import sleep
@@ -15,69 +15,110 @@ from src.clients.player_create import create_player
 from src.clients.room_join import join_room
 from src.config import CONFIG
 
-
-async def roomWebSocket(roomId: str):
-  async with websockets.connect("ws://web:5555/trpc") as websocket:
-    data = json.dumps(
-      {
-        "id": 5,
-        "method": "subscription",
-        "params": {
-          "input": {
-            "json": {
-              "roomId": roomId
+async def roomWebSocket(player_id: str, roomId: str):
+    async with websockets.connect("ws://web:5555/trpc") as websocket:
+        data = json.dumps(
+          {
+            "id": 5,
+            "method": "subscription",
+            "params": {
+              "input": {
+                "json": {
+                  "playerId": player_id,
+                  "roomId": roomId
+                }
+              },
+              "path": "stream.gameStream"
             }
-          },
-          "path": "stream.gameStream"
-        }
-      }
-    )
-    await websocket.send(data)
-    response = await websocket.recv()
-    print(response)
-  return response
+          }
+        )
+        await websocket.send(data)
+
+        try:
+            while True:  # メッセージを聞き続ける
+                response = await websocket.recv()
+                res_dic = json.loads(response)
+                print(res_dic)
+                if(res_dic["result"]["type"] == "data"):
+                    if(res_dic["result"]["data"]["json"]["eventType"] == "changePhase" and res_dic["result"]["data"]["json"]["phase"] == "VOTING"):
+                        print(res_dic["result"]["data"]["json"]["phase"])
+                        res = await send_player_skipPhase(player_id)
+                        print(res)
+                # if(await send_room_state(roomId) == "VOTING"):
+                #     res = await send_player_skipPhase(player_id)
+                #     print(res)
+                
+        except websockets.exceptions.ConnectionClosed:
+            print("接続が閉じられました")
+
+    return 0
 
 def playGame(playerName:str, roomId:str):
     with ApiClient(CONFIG.api_config) as client:
-      instance = DefaultApi(client)
-      player_result = create_player(instance, f"cffnpwr{str(p)}")
-      if is_successful(player_result):
-        player = player_result.unwrap()
+        instance = DefaultApi(client)
+        player_result = create_player(instance, f"cffnpwr{str(p)}")
+        if is_successful(player_result):
+            player = player_result.unwrap()
 
-        room = join_room(instance, player["id"], str(args[1]))
-        pprint(room)
-      else:
-        pprint(player_result.failure())
-        exit(1)
+            room = join_room(instance, player["id"], str(args[1]))
+            pprint(room)
+        else:
+            pprint(player_result.failure())
+            exit(1)
 
-      old_res = None
-      while True:
-        res = asyncio.get_event_loop().run_until_complete(roomWebSocket(room.id))
-        if res != old_res:
-            old_res = res
-            print(res)
-        sleep(1)  # ここで適切なスリープ時間を設定
+        asyncio.get_event_loop().run_until_complete(roomWebSocket(player["id"], room.id))
+
+async def send_player_skipPhase(player_id:str):
+    # ベースURLとエンドポイント
+    base_url = "http://web:5555/api"
+    endpoint = "/game/skipPhase"
+
+    # リクエストボディに含めるJSONデータ
+    json_data = {
+        "playerId": player_id
+    }
+
+    # POSTリクエストを送信
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{base_url}{endpoint}", json=json_data) as response:
+            data = await response.json()
+            return data
+
+async def send_room_state(room_id:str):
+    # ベースURLとエンドポイント
+    base_url = "http://web:5555/api"
+    endpoint = f"/room/{room_id}"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{base_url}{endpoint}") as response:
+            if response.status == 200:
+                data = await response.json()
+                print(data["phase"])
+                return data["phase"]
+            else:
+                # エラーハンドリング
+                return None
 
 if __name__ == "__main__":
-  # 引数1 合言葉 2 人数
-  # 引数0=1の処理
-  args = sys.argv
-  if(len(args) == 1):
-    print("合言葉を入れてください")
-    sys.exit(1)
+    args = sys.argv
+    if len(args) == 1:
+        print("合言葉を入れてください")
+        sys.exit(1)
+    elif len(args) == 2:
+        playerCount = 1
+    elif int(args[2]) > 7:
+        playerCount = 7
+    else:
+        playerCount = int(args[2])
 
-  elif(len(args) == 2):
-      playerCount:int = 1
-  elif(int(args[2]) > 7):
-      playerCount:int = 7
-  else:
-      playerCount:int = int(args[2])
+    players = []
+    for p in range(playerCount):
+        players.append(Process(target=playGame, args=(f"cffnpwr{p+7}", args[1])))
+        players[p].start()
 
-  players = []
-  # playerの生成 正直キモい
-  for p in range(playerCount):
-    players.append(Process(target=playGame, args=(f"cffnpwr{p}", args[1])))
-    players[p].start()
-
-  for p in range(playerCount):
-    players[p].join()
+    try:
+        for p in range(playerCount):
+            players[p].join()
+    except KeyboardInterrupt:
+        print("ユーザーによってプログラムが終了されました")
+        sys.exit(0)
