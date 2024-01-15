833af84916dab4ef21353da5db0937dbc951a4b7
yanbaru1331 <m19034@g.metro-cit.ac.jp>
2024-01-15 03:38:38 +0000

    API関数化したので好きなところで実行できるよ

--- apps/npc/src/api.py
+++ apps/npc/src/api.py
@@ -1,21 +1,188 @@
 from flask import Flask
 import subprocess
 import shlex
+import asyncio
+from urllib import response
+import websockets
+import json, sys, datetime, aiohttp, random
+
+
+from multiprocessing import Process
+from time import sleep
+from os import getpid, getppid
+
+from pprint import pprint
+from caffe_break_client.api_client import ApiClient
+from caffe_break_client.api.default_api import DefaultApi
+from returns.pipeline import is_successful
+from src.clients.player_create import create_player
+from src.clients.room_join import join_room
+from src.config import CONFIG
+
+
 
 app = Flask(__name__)
 
 @app.route('/createnpc/<room_id>/<player_count>', methods=['GET'])
 def start_game(room_id, player_count=1):
-    # 既存のスクリプトを実行するコマンドを準備
-    command = f'python __main__.py {room_id} {player_count}'
-    args = shlex.split(command)
 
     try:
         # サブプロセスを開始
-        subprocess.Popen(args)
+        i = int(player_count)
+        create_npc(i, room_id)
         return {'message': 'ゲームが正常に開始されました'}, 200
     except Exception as e:
         return {'error': str(e)}, 500
 
+
+
+
+async def roomWebSocket(player_id: str, roomId: str):
+    async with websockets.connect("ws://web:5555/trpc") as websocket:
+        # intなら1年運用しないからMDHMSmsでいいんじゃないですかね
+        # 実際長いこれstr使えるならPID+MSでいい気ガス
+        data = json.dumps(
+          {
+        "id": player_id,
+            "method": "subscription",
+            "params": {
+              "input": {
+                "json": {
+                  "playerId": player_id,
+                  "roomId": roomId
+                }
+              },
+              "path": "stream.gameStream"
+
+            }
+          }
+        )
+        await websocket.send(data)
+
+        try:
+            while True:  # メッセージを聞き続ける
+                response = await websocket.recv()
+                res_dic = json.loads(response)
+                print(res_dic)
+                if(res_dic["result"]["type"] == "data"):
+                    match res_dic["result"]["data"]["json"]["eventType"]:
+                        case "changePhase":
+                            match res_dic["result"]["data"]["json"]["phase"]:
+                                case "VOTING":
+                                    res = await rand_voting(roomId, player_id)
+                                    print(res)
+                                case "DISCUSSION":                        
+                                    print(res_dic["result"]["data"]["json"]["phase"])
+                                    res = await send_player_skipPhase(player_id)
+                                    print(res)
+                        case "playerUpdate":
+                            if(res_dic["result"]["data"]["json"]["status"] == "DEAD"):
+                                print("グエー死んだンゴ")
+                                exit()
+                            
+                # if(await send_room_state(roomId) == "VOTING"):
+                #     res = await send_player_skipPhase(player_id)
+                #     print(res)
+                
+        except websockets.exceptions.ConnectionClosed:
+            print("接続が閉じられました")
+
+    return 0
+
+def playGame(playerName:str, roomId:str):
+    with ApiClient(CONFIG.api_config) as client:
+        instance = DefaultApi(client)
+        player_result = create_player(instance, f"{playerName}")
+        if is_successful(player_result):
+            player = player_result.unwrap()
+
+            room = join_room(instance, player["id"], roomId)
+            pprint(room)
+        else:
+            pprint(player_result.failure())
+            exit(1)
+
+        asyncio.get_event_loop().run_until_complete(roomWebSocket(player["id"], room.id))
+
+
+async def send_player_skipPhase(player_id:str):
+    # ベースURLとエンドポイント
+    base_url = "http://web:5555/api"
+    endpoint = "/game/skipPhase"
+
+    # リクエストボディに含めるJSONデータ
+    json_data = {
+        "playerId": player_id
+    }
+
+    # POSTリクエストを送信
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{base_url}{endpoint}", json=json_data) as response:
+            data = await response.json()
+            return data
+
+async def send_room_state(room_id:str):
+    # ベースURLとエンドポイント
+    base_url = "http://web:5555/api"
+    endpoint = f"/room/{room_id}"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{base_url}{endpoint}") as response:
+            if response.status == 200:
+                data = await response.json()
+                print(data["phase"])
+                return data["phase"]
+            else:
+                # エラーハンドリング
+                return None
+
+async def rand_voting(room_id:str, player_id:str):
+    base_url = "http://web:5555/api"
+    endpoint = f"/room/{room_id}"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{base_url}{endpoint}") as response:
+            if response.status == 200:
+                data = await response.json()
+            else:
+                # エラーハンドリング
+                return "Don't get Room State"
+        players = data["players"]
+        targetPlayers = []
+        
+        # 気合の生きているPの取得
+        #プロデューサーじゃないよ
+        for p in players:
+            if(p["status"] == "ALIVE" and p["id"] != player_id):
+                targetPlayers.append(p)
+
+        # リクエストボディに含めるJSONデータ
+        json_data = {
+            "playerId": player_id,
+            "target": targetPlayers[random.randrange(0, len(targetPlayers)-1, 1)]["id"]
+        }
+
+        # POSTリクエストを送信
+        async with session.post(f"{base_url}/game/vote", json=json_data) as response:
+            data = await response.json()
+            return data
+
+
+
+def create_npc(playerCount:int, roomPassword):
+    players = []
+    for p in range(playerCount):
+        players.append(Process(target=playGame, args=(f"cffnpwr{p+7}", roomPassword)))
+        players[p].start()
+
+    try:
+        for p in range(playerCount):
+            players[p].join()
+    except KeyboardInterrupt:
+        print("ユーザーによってプログラムが終了されました")
+        sys.exit(0)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5432)
+    
